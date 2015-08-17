5cfe4d6426ad6a0a00e6ff344c54b5368df82273
sunchuanlei <sunchuanlei@loukou.com>
2015-08-17 11:30:33 +0800

    修改新建框架中的部分问题

--- css-service/src/main/java/com/loukou/css/service/dao/UserDao.java
+++ css-service/src/main/java/com/loukou/css/service/dao/UserDao.java
@@ -2,8 +2,7 @@ package com.loukou.css.service.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.loukou.css.service.service.entity.User;
-
+import com.loukou.css.service.entity.User;
 
 public interface UserDao extends PagingAndSortingRepository<User,Integer>{
 
