0476b4b39bf8b2ab9f4724da48bf37d30cbc8599
sunchuanlei <sunchuanlei@loukou.com>
2015-08-19 19:26:23 +0800

    引用本地调试的order-service

--- css-web/src/main/java/com/loukou/css/web/OrderController.java
+++ css-web/src/main/java/com/loukou/css/web/OrderController.java
@@ -1,11 +1,22 @@
 package com.loukou.css.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.loukou.order.service.api.CssOrderService;
+import com.loukou.order.service.api.OrderService;
+import com.loukou.order.service.req.dto.CssOrderReqDto;
+import com.loukou.order.service.resp.dto.CssOrderRespDto;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+	@Autowired
+	private CssOrderService cssOrderService;
+	
 	@RequestMapping("/allOrder")
 	public String allOrder(){
 		return "orders/OrderIndex";
@@ -14,4 +25,11 @@ public class OrderController {
 //	public String orderDetail(){
 //		
 //	}
+	@RequestMapping("/findOrder")
+	public String queryOrder(Integer pageNum, Integer pageSize, CssOrderReqDto cssOrderReqDto){
+//		List<CssOrderRespDto> resultList = cssOrderService.queryOrderList(pageNum, pageSize, cssOrderReqDto);
+//		System.out.println(resultList);
+		return "";
+		
+	}
 }
