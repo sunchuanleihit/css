92f99113965ead0103ec6d3c3a42511d918b3fcb
wr@loukou.com <wr@loukou.com>
2015-08-21 10:23:28 +0800

    update

--- css-web/src/main/java/com/loukou/css/web/OrderController.java
+++ css-web/src/main/java/com/loukou/css/web/OrderController.java
@@ -1,55 +1,56 @@
 package com.loukou.css.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.loukou.css.annotation.AuthPassport;
 import com.loukou.css.util.DataGrid;
-import com.loukou.order.service.api.CssOrderService;
+import com.loukou.order.service.api.BkOrderService;
 import com.loukou.order.service.req.dto.CssOrderReqDto;
+import com.loukou.order.service.resp.dto.BkOrderListDto;
+import com.loukou.order.service.resp.dto.BkOrderListRespDto;
 import com.loukou.order.service.resp.dto.CssOrderRespDto;
+import com.loukou.order.service.resp.dto.OrderListDto;
+import com.loukou.order.service.resp.dto.OrderListRespDto;
 
 @Controller
 @RequestMapping("/order")
 @AuthPassport
 public class OrderController {
 	@Autowired
-	private CssOrderService cssOrderService;
+	private BkOrderService bkOrderService;
 	
 	@RequestMapping("/allOrder")
 	public String allOrder(){
 		return "orders/OrderIndex";
 	}
 	
-	@RequestMapping(value = "/orderDetail/{orderNo}", method = RequestMethod.GET)
-	public String orderDetail(@PathVariable String orderNo){
-		return "orders/OrderDetail";
+	@RequestMapping(value = "/orderDetail/{orderSnMain}", method = RequestMethod.GET)
+	public ModelAndView orderDetail(@PathVariable String orderSnMain,
+			ModelMap modelMap) {
+		ModelAndView mv = new ModelAndView("orders/OrderDetail");
+		BkOrderListRespDto orderDetail = bkOrderService.orderDetail(orderSnMain);
+		
+		if(orderDetail.getCode()==200){			
+			List<BkOrderListDto> orderDetailMsgs = orderDetail.getResult().getOrderList();
+			modelMap.put("operateInfo", orderDetailMsgs);
+		}
+		return mv;
 	}
 	
-//	@RequestMapping(value = "/orderDetail/{orderNo}", method = RequestMethod.GET)
-//	public ModelAndView orderDetail(@PathVariable String orderNo,
-//			ModelMap modelMap) {
-//		ModelAndView mv = new ModelAndView("orders/OrderDetail");
-//		OrderDetailBo purchase = purchaseProcessor.getPurchaseDeatil(orderNo);
-//		List<PurchaseDetailBo> purchaseList = purchase.getPurchaseDetailBo();
-//		BaseRes<OperateListBo> operateList = stockoutProcessor
-//				.getOperateInfoByOrderId(Integer.parseInt(orderNo),
-//						OperateVoucherTypeEnum.Purchase_Order.getId());
-//		mv.addObject("purchase", purchase);
-//		mv.addObject("purchaseList", purchaseList);
-//		modelMap.put("operateInfo", operateList.getResult().getOperateBo());
-//		return mv;
-//	}
 	@RequestMapping("/findOrder")
 	@ResponseBody
 	public DataGrid queryOrder(int page, int rows, CssOrderReqDto cssOrderReqDto){
-		List<CssOrderRespDto> resultList = cssOrderService.queryOrderList(page, rows, cssOrderReqDto);
+		List<CssOrderRespDto> resultList = bkOrderService.queryOrderList(page, rows, cssOrderReqDto);
 		DataGrid grid = new DataGrid();
 		grid.setTotal(1574641);
 		grid.setRows(resultList);
